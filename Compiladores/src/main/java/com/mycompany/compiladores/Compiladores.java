1445f55dc93c1a516fb6fd338852788478996ff7
Uriel Hernandez <juanhernandez290402@gmail.com>
2023-10-02 18:04:44 -0600

    Ventana

--- Compiladores/src/main/java/com/mycompany/compiladores/Compiladores.java
+++ Compiladores/src/main/java/com/mycompany/compiladores/Compiladores.java
@@ -4,9 +4,11 @@ package com.mycompany.compiladores;
  *
  * @author uriel
  */
+
 public class Compiladores {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Ventana newframe = new Ventana(); 
+        newframe.setVisible(true);
     }
 }
