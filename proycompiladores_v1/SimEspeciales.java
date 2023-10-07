7a015fb2888e20b4b376f8673e6c5f2379b60c80
Uriel Iturbide <146056495+Codeccino01@users.noreply.github.com>
2023-10-07 14:35:55 -0600

    Add files via upload

---
+++ proycompiladores_v1/SimEspeciales.java
@@ -0,0 +1,8 @@
+import java.io.Serializable;
+
+public class SimEspeciales implements Serializable
+{
+	public static char EPSILON = (char)5;
+	public static char FIN = (char)0;
+	public static int TOKENERROR = 9999;
+}
---
+++ proycompiladores_v1/main.java
@@ -0,0 +1,8 @@
+public class main
+{
+    public static void main(String args[])
+    {            
+        ventana v1 = new ventana();                
+        v1.setVisible(true);      
+    }	
+}
